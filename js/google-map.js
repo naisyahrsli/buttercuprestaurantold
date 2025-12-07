2c0edbe581d85f3f90d48f3743f9cd93409d6711
naisyahrsli <aisyah.rosli04@gmail.com>
2025-12-07 15:55:34 +0800

    Add files via upload

---
+++ js/google-map.js
@@ -0,0 +1,204 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(3.085809, 101.748428);
+    // 39.399872
+    // -8.224454
+    
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 7,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+        styles: [
+				  {
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#f5f5f5"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.icon",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#616161"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.text.stroke",
+				    "stylers": [
+				      {
+				        "color": "#f5f5f5"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative.land_parcel",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#bdbdbd"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#eeeeee"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#757575"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.park",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#e5e5e5"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.park",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#9e9e9e"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#ffffff"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.arterial",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#757575"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#dadada"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#616161"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.local",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#9e9e9e"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.line",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#e5e5e5"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.station",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#eeeeee"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#c9c9c9"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#9e9e9e"
+				      }
+				    ]
+				  }
+				]
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    // Koordinat restoran
+var myLatlng = new google.maps.LatLng(3.085809, 101.748428); // tukar ikut lokasi
+
+// Buat marker terus
+new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: "Buttercup Restaurant",
+    icon: 'images/loc.png' // kalau nak icon custom
+});
+    
+}
+google.maps.event.addDomListener(window, 'load', init);
